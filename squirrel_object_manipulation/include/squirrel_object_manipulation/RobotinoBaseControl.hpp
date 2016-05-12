05b949ed5a75d2f2d6e89e728e261cc856e5a620
Senka Krivic <Senka.Krivic@uibk.ac.at>
2016-05-12 18:56:16 +0200

    added move basce controller class for arm controlling

---
+++ squirrel_object_manipulation/include/squirrel_object_manipulation/RobotinoBaseControl.hpp
@@ -0,0 +1,57 @@
+#ifndef ROBOTINOBASECONTROL
+#define ROBOTINOBASECONTROL
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <boost/thread.hpp>
+#include <control_toolbox/pid.h>
+#include <tf/tf.h>
+
+#define ROBOTINO_MOVE_TOPIC "/cmd_vel"
+#define ROBOTINO_ODOM_TOPIC "/odom"
+
+
+class RobotinoBaseControl {
+
+private:
+
+    ros::NodeHandle  private_nh;
+
+    ros::Subscriber subOdometry;
+    ros::Publisher pubMove;
+
+    nav_msgs::Odometry odometry;
+
+    boost::thread* move_base_thread_;
+    bool start_move_base_;
+
+    double controller_frequency_, time_step_;
+    double vel_ang_max_;
+    double p_theta_, d_theta_, i_theta_, i_theta_min_, i_theta_max_;
+
+    double desired_theta_;
+
+    control_toolbox::Pid pid_theta_;
+    geometry_msgs::Twist current_base_vel_;
+
+    boost::mutex robot_pose_mutex_;
+    boost::mutex move_pose_mutex_;
+
+    void callbackOdometry(nav_msgs::Odometry msg);
+    void moveBaseThread();
+
+    geometry_msgs::Twist getNullTwist();
+    double rotationDifference(double angle, double theta_robot);
+
+
+public:
+
+    RobotinoBaseControl(ros::NodeHandle& node, double  controller_frequency_, double max_ang_vel_);
+    ~RobotinoBaseControl();
+
+    void move(double desired_theta);
+
+};
+
+#endif
