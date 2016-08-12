f977b37bd0f2b8840aef76bac68dcf9cee013316
shangl <simon.hangl@uibk.ac.at>
2016-08-12 12:08:29 +0200

    added joint ptp

--- squirrel_ptp_server/src/ptp_node.cpp
+++ squirrel_ptp_server/src/ptp_node.cpp
@@ -1,12 +1,14 @@
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
 #include <squirrel_manipulation_msgs/PtpAction.h>
+#include <squirrel_manipulation_msgs/JointPtpAction.h>
 
 #include <vector>
 #include <memory>
 #include <iostream>
 #include <kukadu/kukadu.hpp>
 #include <cmath>
+#include <mutex>
 
 using namespace std;
 using namespace arma;
@@ -15,22 +17,35 @@ using namespace kukadu;
 class PtpAction {
 protected:
 
+	bool ptpRunning;
+	std::mutex ptpMutexRunning;
+
 	ros::NodeHandle nh_;
 	// NodeHandle instance must be created before this line. Otherwise strange error may occur.
-	actionlib::SimpleActionServer<squirrel_manipulation_msgs::PtpAction> as_; 
+	actionlib::SimpleActionServer<squirrel_manipulation_msgs::PtpAction> as_;
+	actionlib::SimpleActionServer<squirrel_manipulation_msgs::JointPtpAction> jointPtpAs_;
 	std::string action_name_;
+	std::string jointPtpName;
 	// create messages that are used to published feedback/result
 	squirrel_manipulation_msgs::PtpFeedback feedback_;
 	squirrel_manipulation_msgs::PtpResult result_;
+	
+	squirrel_manipulation_msgs::JointPtpFeedback jptpfeedback_;
+	squirrel_manipulation_msgs::JointPtpResult jptpresult_;
 	
 	KUKADU_SHARED_PTR<KukieControlQueue> robotinoQueue;
 	KUKADU_SHARED_PTR<MoveItKinematics> mvKin;
 
 public:
 
-	PtpAction(std::string name) :
+	PtpAction(std::string name, std::string jointPtpName) :
 		as_(nh_, name, boost::bind(&PtpAction::executeCB, this, _1), false),
+		jointPtpAs_(nh_, jointPtpName, boost::bind(&PtpAction::executeJointPtp, this, _1), false),
 		action_name_(name) {
+			
+		this->jointPtpName = jointPtpName;
+		
+		ptpRunning = false;
 			
 		cout << "setting up control queue" << endl;
 		robotinoQueue = KUKADU_SHARED_PTR<KukieControlQueue>(new KukieControlQueue("real", "robotino", nh_));
@@ -46,34 +61,79 @@ public:
 		auto realLqThread = robotinoQueue->startQueue();
 		
 		as_.start();
+		jointPtpAs_.start();
 		
 	}
 
 	~PtpAction(void) {
 	}
+	
+	void executeJointPtp(const squirrel_manipulation_msgs::JointPtpGoalConstPtr& goal) {
+		
+		ptpRunning = true;
+		
+		if(!ptpRunning) {
+		
+			if(robotinoQueue->getCurrentMode() != KukieControlQueue::KUKA_JNT_POS_MODE) {
+				robotinoQueue->stopCurrentMode();
+				robotinoQueue->switchMode(KukieControlQueue::KUKA_JNT_POS_MODE);
+			}
+			
+			jptpfeedback_.current_status = "start planning";
+			jointPtpAs_.publishFeedback(jptpfeedback_);
+			robotinoQueue->jointPtp(goal->joints.data);
+			
+			robotinoQueue->stopCurrentMode();
+			
+			jptpresult_.result_status = "execution done";
+			jointPtpAs_.setSucceeded(jptpresult_);
+			
+		} else {
+			
+			jptpresult_.result_status = "execution failed because another ptp is currently running";
+			jointPtpAs_.setSucceeded(jptpresult_);
+			
+		}
+		
+		ptpRunning = false;
+		
+	}
 
 	void executeCB(const squirrel_manipulation_msgs::PtpGoalConstPtr &goal) {
 		
-		if(robotinoQueue->getCurrentMode() != KukieControlQueue::KUKA_JNT_POS_MODE) {
-			robotinoQueue->stopCurrentMode();
-			robotinoQueue->switchMode(KukieControlQueue::KUKA_JNT_POS_MODE);
-		}
+		ptpRunning = true;
 		
-		feedback_.current_status = "start planning";
-		as_.publishFeedback(feedback_);
-		auto jointPlan = mvKin->planCartesianTrajectory({goal->pose});
+		if(!ptpRunning) {
 		
-		if(jointPlan.size() > 5) {
-			feedback_.current_status = "start execution";
+			if(robotinoQueue->getCurrentMode() != KukieControlQueue::KUKA_JNT_POS_MODE) {
+				robotinoQueue->stopCurrentMode();
+				robotinoQueue->switchMode(KukieControlQueue::KUKA_JNT_POS_MODE);
+			}
+			
+			feedback_.current_status = "start planning";
 			as_.publishFeedback(feedback_);
-			robotinoQueue->setNextTrajectory(jointPlan);
-			robotinoQueue->synchronizeToQueue(1);
+			auto jointPlan = mvKin->planCartesianTrajectory({goal->pose});
+			
+			if(jointPlan.size() > 5) {
+				feedback_.current_status = "start execution";
+				as_.publishFeedback(feedback_);
+				robotinoQueue->setNextTrajectory(jointPlan);
+				robotinoQueue->synchronizeToQueue(1);
+			}
+			
+			robotinoQueue->stopCurrentMode();
+			
+			result_.result_status = "execution done";
+			as_.setSucceeded(result_);
+			
+		} else {
+			
+			jptpresult_.result_status = "execution failed because another ptp is currently running";
+			jointPtpAs_.setSucceeded(jptpresult_);
+			
 		}
 		
-		robotinoQueue->stopCurrentMode();
-		
-		result_.result_status = "execution done";
-		as_.setSucceeded(result_);
+		ptpRunning = false;
 
 	}
 
@@ -82,10 +142,10 @@ public:
 
 int main(int argc, char** argv) {
 
-	ros::init(argc, argv, "Ptp");
+	ros::init(argc, argv, "ptp");
 	sleep(1); ros::AsyncSpinner spinner(10); spinner.start();
 
-	PtpAction ptp(ros::this_node::getName());
+	PtpAction ptp("cart_ptp", "joint_ptp");
 	
 	getchar();
 
