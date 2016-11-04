8927264f4ee2db90db9b83671de03ae996d18f9c
Philipp Zech <philipp.zech@uibk.ac.at>
2016-11-04 08:26:22 +0100

    bare grasp learning ROS package

--- squirrel_grasp_learning/scripts/squirrel_grasp_learner_node.py
+++ squirrel_grasp_learning/scripts/squirrel_grasp_learner_node.py
@@ -2,13 +2,16 @@
 
 import rospy
 
-from squirrel_grasp_learning.learner import MHGraspLearner
+#from squirrel_grasp_learning.learner import MHGraspLearner
+from squirrel_grasp_learning.learner import test
 
 
 if __name__ == '__main__':
     try:
         rospy.init_node('squirrel_grasp_learning_node')
-        BGS = MHGraspLearner()
+        #MHGL = MHGraspLearner()
+        tester = test.Test()
+        tester.run()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
